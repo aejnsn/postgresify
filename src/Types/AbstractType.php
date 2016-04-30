b5cc478d54a79b0c19fa48eec71a9182bcfc2e05
Aaron Johnson <ajohnson@kimmel.com>
2016-04-30 10:40:49 -0400

    Implement getter/setter dynamic properties.

--- src/Types/AbstractType.php
+++ src/Types/AbstractType.php
@@ -2,6 +2,8 @@
 
 namespace Aejnsn\Postgresify\Types;
 
+use Exception;
+
 abstract class AbstractType
 {
     /**
@@ -10,4 +12,39 @@ abstract class AbstractType
      * @return string
      */
     abstract public function __toString();
+
+    /**
+     * Get the type's properties dynamically.
+     *
+     * @param $name
+     *
+     * @return mixed
+     * @throws Exception
+     */
+    public function __get($name)
+    {
+        $method = camel_case('get_' . $name);
+        if (method_exists($this, $method)) {
+            return $this->$method();
+        }
+        throw new Exception("Property {$name} does not exist.");
+    }
+
+    /**
+     * Set the type's properties dynamically.
+     *
+     * @param string $name
+     * @param mixed $value
+     *
+     * @return mixed
+     * @throws Exception
+     */
+    public function __set($name, $value)
+    {
+        $method = camel_case('set_' . $name);
+        if (method_exists($this, $method)) {
+            $this->$method($value);
+        }
+        throw new Exception("Property {$name} does not exist.");
+    }
 }
